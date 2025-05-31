0f740ce79c41cdde1784ab22c95623201204a499
ktzazz <155502502+ktzazz@users.noreply.github.com>
2025-05-31 17:37:38 -0600

    script almost done

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 
 // Mobile Navigation GOT IT FROM W3SCHOOLS
 //https://www.w3schools.com/howto/howto_css_menu_icon.asp
-function myFunction(x) {
-  x.classList.toggle("change");
+const menuButton = document.querySelector(".container"); // Button 
+const mobileMenuContent = document.getElementById("sidenav"); // menu content
+const mobileMenuContainer = document.querySelector(".mobile-menu"); // div containing menu content for aria label
+const overlay = document.querySelector(".overlay");
+
+// 2. Define la función que manejará el clic
+function toggleMobileMenu() {
+    menuButton.classList.toggle("change");
+    const isOpen = menuButton.classList.contains("change"); // verify is menu is open
+
+    if (isOpen) {
+        mobileMenuContent.classList.toggle("open");  // opens the menu
+        overlay.classList.add("active"); // show the overlay
+    } else {
+        mobileMenuContent.style.width = "0"; // closes the menu
+        overlay.classList.remove("active"); // hide the overlay
+    }
+
+    mobileMenuContainer.setAttribute("aria-expanded", isOpen); // set aria attribute to open
+}
+
+menuButton.addEventListener("click", toggleMobileMenu);
+
+// closes the menu if the overlay is clicked
+if (overlay) { // verifies if the overlay exists
+    overlay.addEventListener("click", toggleMobileMenu); // reuses the toggleMobileMenu function to close
 }
